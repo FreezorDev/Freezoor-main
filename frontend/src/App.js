c4d6f5cba460d9242a041c5863b048c24bf0ac13
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2023-04-03 17:57:01 +0530

    v1 frontend with react tailwind + router + redux setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
